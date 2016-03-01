3e66363c6a39e21d122bc0beb017e8e502f45aa7
Valentin C <valentindu64@hotmail.fr>
2016-03-01 13:49:52 +0100

    Version 1.6
    Update view

--- app/src/main/java/net/valentinc/EssenceDatabase/result.java
+++ app/src/main/java/net/valentinc/EssenceDatabase/result.java
@@ -7,21 +7,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.ParseException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
+import android.widget.Toast;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  * Created by valentin on 25/06/15.
@@ -30,17 +28,12 @@ import java.util.ArrayList;
 public class result extends Activity {
     Button btnRafraichir = null;
     Button btnRetour = null;
-    TextView TextKmMoyen = null;
+
     TextView TextKmTotal = null;
-    TextView TextKmDernier = null;
-
-    TextView TextEssenceMoyen = null;
-    TextView TextEssenceDernier = null;
     TextView TextEssenceTotal = null;
-
-    TextView TextPrixMoyen= null;
-    TextView TextPrixDernier = null;
     TextView TextPrixTotal = null;
+
+    TextView TextPrixCent = null;
     TextView TextPrixKm = null;
     TextView TextPrixCovoit = null;
 
@@ -55,18 +48,14 @@ public class result extends Activity {
         // on récupère les edittext et les boutons
         btnRafraichir = (Button) findViewById(R.id.buttonRafraichir);
         btnRetour  = (Button) findViewById(R.id.buttonRetour);
-        TextKmMoyen= (TextView) findViewById(R.id.textViewKmMoyen);
-        TextKmDernier = (TextView) findViewById(R.id.textViewKmDernier);
+
         TextKmTotal = (TextView) findViewById(R.id.textViewKmTotal);
-
-        TextEssenceMoyen = (TextView) findViewById(R.id.textViewEssenceMoyenPlein);
-        TextEssenceDernier = (TextView) findViewById(R.id.textViewEssenceDernierPlein);
         TextEssenceTotal = (TextView) findViewById(R.id.EssenceTotal);
-
-        TextPrixDernier = (TextView) findViewById(R.id.textViewPrix);
-        TextPrixMoyen = (TextView) findViewById(R.id.textViewPrixMoyen);
-        TextPrixKm = (TextView) findViewById(R.id.textViewPrixKm);
         TextPrixTotal = (TextView) findViewById(R.id.textViewPrixTotal);
+
+        TextPrixCent = (TextView) findViewById(R.id.textViewL100);
+        TextPrixKm = (TextView) findViewById(R.id.textViewPrixKm);
+
         TextPrixCovoit = (TextView) findViewById(R.id.PrixCovoit);
 
         TextViewInfo = (TextView) findViewById(R.id.TextViewInfo);
@@ -97,16 +86,13 @@ public class result extends Activity {
             public void run() {
                 InputStream is = null;
                 String result = new String();
-                ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
                 donnees = new ArrayList<>();
 
                 try {
-                    HttpClient httpclient = new DefaultHttpClient();
-                    HttpPost httppost = new HttpPost("http://88.142.52.11/android/getdata.php");
-                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                    HttpResponse response = httpclient.execute(httppost);
-                    HttpEntity entity = response.getEntity();
-                    is = entity.getContent();
+                    URL url = new URL("http://88.142.52.11/android/getdata.php");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    is = conn.getInputStream();
+
 
                     //conversion de la réponse en chaine de caractère
                     try {
@@ -124,24 +110,23 @@ public class result extends Activity {
                     //recuperation des donnees json
                     try {
                         JSONObject jsonObj = new JSONObject(result);
-                        donnees.add(jsonObj.get("akm").toString() + " km");
-                        donnees.add(jsonObj.get("lkm").toString() + " km");
-                        donnees.add(jsonObj.get("al").toString() + " litres");
-                        donnees.add(jsonObj.get("ll").toString() + " litres");
-                        donnees.add(jsonObj.get("tl").toString() + " litres");
-                        donnees.add(jsonObj.get("ap").toString() + " €");
-                        donnees.add(jsonObj.get("lp").toString() + " €");
-                        donnees.add(jsonObj.get("pkm").toString() + " €");
-                        donnees.add(jsonObj.get("kmt").toString() + " km");
-                        donnees.add(jsonObj.get("pt").toString() + " €");
-                        donnees.add(jsonObj.get("pc").toString() + " €");
+                        donnees.add(NumberFormat.getNumberInstance(Locale.FRANCE).format(Float.parseFloat(jsonObj.get("kmt").toString())));
+                        donnees.add(NumberFormat.getNumberInstance(Locale.FRANCE).format(Float.parseFloat(jsonObj.get("lt").toString())));
+                        donnees.add(NumberFormat.getNumberInstance(Locale.FRANCE).format(Float.parseFloat(jsonObj.get("pt").toString())));
+                        donnees.add(NumberFormat.getNumberInstance(Locale.FRANCE).format(Float.parseFloat(jsonObj.get("pkm").toString())));
+                        donnees.add(NumberFormat.getNumberInstance(Locale.FRANCE).format(Float.parseFloat(jsonObj.get("lcent").toString())));
+                        donnees.add(NumberFormat.getNumberInstance(Locale.FRANCE).format(Float.parseFloat(jsonObj.get("pc").toString())));
                         donnees.add(jsonObj.get("date").toString());
                         donnees.add(jsonObj.get("nbr").toString());
 
                     } catch (JSONException e) {
                         Log.i("tagjsonexp", "" + e.toString());
-                    } catch (ParseException e) {
-                        Log.i("tagjsonpars", "" + e.toString());
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(),"Error while parsing",Toast.LENGTH_SHORT);
+                            }
+                        });
                     }
                     Log.i("pass 1", "connection success ");
 
@@ -156,17 +141,12 @@ public class result extends Activity {
     }
 
     public void update_view() {
-        TextKmMoyen.setText(donnees.get(0));
-        TextKmDernier.setText(donnees.get(1));
-        TextEssenceMoyen.setText(donnees.get(2));
-        TextEssenceDernier.setText(donnees.get(3));
-        TextEssenceTotal.setText(donnees.get(4));
-        TextPrixMoyen.setText(donnees.get(5));
-        TextPrixDernier.setText(donnees.get(6));
-        TextPrixKm.setText(donnees.get(7));
-        TextKmTotal.setText(donnees.get(8));
-        TextPrixTotal.setText(donnees.get(9));
-        TextPrixCovoit.setText(donnees.get(10));
-        TextViewInfo.setText("Depuis :  " + donnees.get(11) + "     avec    " + donnees.get(12) + "  échantillons. ");
+        TextKmTotal.setText(donnees.get(0));
+        TextEssenceTotal.setText(donnees.get(1));
+        TextPrixTotal.setText(donnees.get(2));
+        TextPrixKm.setText(donnees.get(3));
+        TextPrixCent.setText(donnees.get(4));
+        TextPrixCovoit.setText(donnees.get(5));
+        TextViewInfo.setText("Depuis :  " + donnees.get(6) + "     avec    " + donnees.get(7) + "  échantillons. ");
     }
 }
